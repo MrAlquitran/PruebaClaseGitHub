2230c0482097702235c8c91d1afeb2f530672b93
Pablo <pablopemar11@gmail.com>
2024-10-25 12:33:41 +0200

    Ejercicio 4 necesita acabar una funcion, iniciar E9

--- WS#1/E4.js
+++ WS#1/E4.js
@@ -1,5 +1,7 @@
-function tabla(){
-    document.write("<table border = "+1+">");
+    window.onload = tabla();
+        function tabla(){
+        document.write("<table border = "+1+">");
+
     for (let i = 0; i < 100; i++) {
         document.write("<tr width=5px height = 5px>");
         for (let k = 0; k < 100; k++) {
@@ -9,9 +11,57 @@ function tabla(){
     }
     document.write("</table>");
 
-    var celda = document.getElementsByTagName("td");
+    const celda = document.querySelectorAll("td");
+  
+
+    celda.forEach(celda => { celda.addEventListener ("mouseover", function(dibujar){
+        if (dibujar.altKey){
+            celda.style.backgroundColor = "red"
+        }
+        else if (dibujar.ctrlKey){
+            celda.style.backgroundColor = "blue"
+        }
+        else if (dibujar.shiftKey){
+            celda.style.backgroundColor = "white"
+        }
+    })});
+}
     
 
+
+
+const btn = document.getElementsByTagName("button");
+btn[0].addEventListener("click", function limpiar(){
+    document.write("<table border = "+1+">");
+for (let i = 0; i < 100; i++) {
+    document.write("<tr width=5px height = 5px>");
+    for (let k = 0; k < 100; k++) {
+        document.write("<td width=5px height = 5px></td>");
+    }
+    document.write("</tr>")
+}
+document.write("</table>");
+
+})    
+const celda = document.querySelectorAll("td");
+  
+
+    celda.forEach(celda => { celda.addEventListener ("mouseover", function(dibujar){
+        if (dibujar.altKey){
+            celda.style.backgroundColor = "red"
+        }
+        else if (dibujar.ctrlKey){
+            celda.style.backgroundColor = "blue"
+        }
+        else if (dibujar.shiftKey){
+            celda.style.backgroundColor = "white"
+        }
+    })});
+
+
+
+
+/*
     if ("keydown" == "Shift") {
         document.addEventListener("keydown",()=>{
             document.addEventListener("mousemove",()=>{
@@ -42,5 +92,5 @@ function tabla(){
                 document.body.style.backgroundColor = "blue";
             })
         });
-    }
-}
+    }*/
+
