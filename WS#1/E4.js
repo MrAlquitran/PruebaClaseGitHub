709ed6f5dbe92f17dc35e995049e12f8063daeb6
Pablo <pablopemar11@gmail.com>
2024-10-25 09:32:42 +0200

    Actualización ejercicio E4

--- WS#1/E4.js
+++ WS#1/E4.js
@@ -1,1 +1,46 @@
-document.write("<table>");
+function tabla(){
+    document.write("<table border = "+1+">");
+    for (let i = 0; i < 100; i++) {
+        document.write("<tr width=5px height = 5px>");
+        for (let k = 0; k < 100; k++) {
+            document.write("<td width=5px height = 5px></td>");
+        }
+        document.write("</tr>")
+    }
+    document.write("</table>");
+
+    var celda = document.getElementsByTagName("td");
+    
+
+    if ("keydown" == "Shift") {
+        document.addEventListener("keydown",()=>{
+            document.addEventListener("mousemove",()=>{
+                document.body.style.backgroundColor = "blue";
+            })
+        });    
+    } else if ("keydown" == "Control") {
+        document.addEventListener("keydown",()=>{
+            document.addEventListener("mousemove",()=>{
+                document.body.style.backgroundColor = "blue";
+            })
+        });
+    }
+    
+    document.addEventListener("mousemove", function(seguir){
+        const y = seguir.clientY
+        const x = seguir.clientX
+        console.log(x,y);});
+    if ("keydown" == "Shift") {
+        document.addEventListener("keydown",()=>{
+            document.addEventListener("mousemove",()=>{
+                document.body.style.backgroundColor = "blue";
+            })
+        });    
+    } else if ("keydown" == "Control") {
+        document.addEventListener("keydown",()=>{
+            document.addEventListener("mousemove",()=>{
+                document.body.style.backgroundColor = "blue";
+            })
+        });
+    }
+}
