34089cb0e32925609e1598a7a460024345b2b2b6
Pablo <pablopemar11@gmail.com>
2024-12-18 10:59:45 +0100

    Separacion por trimestres

---
+++ Segundo trimestre/Jquery.js
@@ -0,0 +1,7 @@
+/*Crear una tabla con 8 filas, las dos primeras tienen que ser rojas, las que se encuentran antes de la tercera, 
+las 2 ultimas azules, las que se encuentran debajo de su respectiva tercera*/ 
+
+window.onload = () =>{
+    $('tr').css({'color':'red'});
+    
+}
